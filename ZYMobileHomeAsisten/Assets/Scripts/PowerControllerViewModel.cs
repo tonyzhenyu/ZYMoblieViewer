6c9d2f574d6e9d8676af295fad86725a3a72bc4f
tonyzhenyu <zfish97@outlook.com>
2024-05-29 01:04:28 +0800

    update model

--- ZYMobileHomeAsisten/Assets/Scripts/PowerControllerViewModel.cs
+++ ZYMobileHomeAsisten/Assets/Scripts/PowerControllerViewModel.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using ZY.MVVM;
 
+public class PowerControllerModel
+{
+    
+}
 public class PowerControllerViewModel : ViewModelBase
 {
+
     public readonly BindableProperty<string> ipAddressInputField = new BindableProperty<string>();
     public readonly BindableProperty<string> ipAddressPortInputField = new BindableProperty<string>();
     public readonly BindableProperty<string> macAddressInputField = new BindableProperty<string>();
