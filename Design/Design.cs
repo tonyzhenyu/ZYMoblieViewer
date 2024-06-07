c5283f8f886c6f400fe41ab4d33a3c4af85390f6
zfub <zfish97@outlook.com>
2024-06-07 18:36:36 +0800

    更新数据设计

--- Design/Design.cs
+++ Design/Design.cs
@@ -22,5 +22,33 @@ namespace MobileViewer{
     {
         public T Encoding(Message<T> input);
     }
+
+
+
 }
 
+
+
+namespace Server{
+
+    public interface ServiceHandler{
+        void Send();
+        void Recive();
+    }
+    public interface Service{
+            
+    }
+    public class Server{
+        public IpAddress ip;
+        public string name;
+        public Service[] services;
+    }
+
+    public class DataBase : Service{
+        
+    }
+    public class SMBFileSystem : Service{
+
+    }
+
+}
